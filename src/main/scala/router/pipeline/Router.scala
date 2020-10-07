a565fc92b29bccb6c67cce6ea8899fa742660ae5
Runji Wang <wangrunji0408@163.com>
2020-10-07 17:09:59 +0800

    hard code config inside Router

--- src/main/scala/router/pipeline/Router.scala
+++ src/main/scala/router/pipeline/Router.scala
@@ -6,11 +6,18 @@ import router._
 import router.table._
 
 class Router extends Pipeline {
+  // config
+  val config = Wire(new RouterConfig())
+  for ((iface, i) <- config.iface.zipWithIndex) {
+    iface.ipv4 := Ipv4Addr(s"10.0.$i.1")
+    iface.mask := Ipv4Addr("255.255.255.0")
+    iface.mac := MacAddr(s"RJGG_$i".map(_.toByte).toArray)
+  }
 
   // the output of last pipeline
   var in = io.in
   def addPipeline[T <: Pipeline](m: T): T = {
-    m.io.config <> io.config
+    m.io.config <> config
     in <> m.io.in
     in = m.io.out
     m
